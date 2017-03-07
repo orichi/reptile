23ed62c1ebd424dea17cbd3d5fd0dc43c5b3b81f
royan <wang2365@126.com>
2017-03-07 18:49:19 +0800

    加载不带load的

--- app/controllers/reptiles_controller.rb
+++ app/controllers/reptiles_controller.rb
@@ -8,7 +8,7 @@ class ReptilesController < ApplicationController
     # url = #session[:info][params[:id]] TODO DB获取存储的链接信息
 
     cat_record = CatRecord.find(params[:id])
-    cat_record.product_load
+    # cat_record.product_load
     @hash = ProdRecord.where(cat_record: cat_record).paginate :page => params[:page], :per_page => 20
   end
 end
